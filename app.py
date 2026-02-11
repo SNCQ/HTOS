1e2e0ec7fbb8e3020c25a84fbc415373f5638226
SNCQ <110749291+SNCQ@users.noreply.github.com>
2026-02-11 17:28:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -58,4 +58,74 @@ if __name__ in {"__main__", "__mp_main__"}:
         workspace_opt.ignore_startup = True
     startup(workspace_opt, lite=True)
 
-    ui.run(root=initialize_tabs, reload=args.reload, native=True, dark=True, window_size=(1400, 800))
+import multiprocessing
+multiprocessing.set_start_method("spawn", force=True)
+
+from dotenv import load_dotenv
+load_dotenv()
+
+import argparse
+from nicegui import ui
+from app_core import models
+from app_core.profile_selector import ProfileSelector
+from app_core.setting_selector import SettingSelector
+from app_core.resign import Resign
+from app_core.encrypt import Encrypt
+from app_core.decrypt import Decrypt
+from app_core.reregion import Reregion
+from app_core.createsave import Createsave
+from app_core.convert import Convert
+from app_core.quickcodes import QuickCodes
+from app_core.sfo_editor import SFOEditor
+from utils.constants import APP_PROFILES_PATH, APP_SETTINGS_PATH, VERSION
+from utils.workspace import WorkspaceOpt, startup, check_version
+
+workspace_opt = WorkspaceOpt()
+
+async def initialize_tabs() -> None:
+    status = await check_version()
+    profiles = models.Profiles(APP_PROFILES_PATH)
+    settings = models.Settings(APP_SETTINGS_PATH)
+
+    with ui.header().classes("h-12 justify-center"):
+        ui.label(f"HTOS {VERSION} ({status})").style("font-size: 15px; font-weight: bold;")
+
+    with ui.tabs().classes("w-full") as tabs:
+        tab_container = [
+            ProfileSelector(profiles),
+            Resign(profiles, settings),
+            Decrypt(settings),
+            Encrypt(profiles, settings),
+            Reregion(profiles, settings),
+            Createsave(profiles, settings),
+            Convert(settings),
+            QuickCodes(settings),
+            SFOEditor(),
+            SettingSelector(settings)
+        ]
+    with ui.tab_panels(tabs, value=tab_container[0].tab).classes("w-full"):
+        for t in tab_container:
+            with ui.tab_panel(t.tab):
+                t.construct()
+
+if __name__ in {"__main__", "__mp_main__"}:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ignore-startup", action="store_true")
+    parser.add_argument("--reload", action="store_true")
+    args, _ = parser.parse_known_args()
+    if args.ignore_startup:
+        workspace_opt.ignore_startup = True
+    startup(workspace_opt, lite=True)
+
+async def startup_ui():
+    await initialize_tabs()
+
+ui.timer(0, startup_ui, once=True)
+
+ui.run(
+    reload=args.reload,
+    native=True,
+    dark=True,
+    window_size=(1400, 800)
+)
+
