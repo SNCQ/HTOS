ab3f94320464ba0243a6571258789cfe46f3130e
SNCQ <110749291+SNCQ@users.noreply.github.com>
2026-04-26 00:16:16 +0300

    Update fc5_crypt.py

--- data/crypto/fc5_crypt.py
+++ data/crypto/fc5_crypt.py
@@ -12,5 +12,7 @@ class Crypt_FC5:
             await cc.write_checksum(crc, 0xC)
 
     @staticmethod
-    async def check_enc_ps(filepath: str) -> None:
-        await Crypt_FC5.encrypt_file(filepath)
+    async def check_enc_ps(folderpath: str) -> None:
+        files = await CC.obtain_files(folderpath)
+        for filepath in files:
+            await Crypt_FC5.encrypt_file(filepath)
